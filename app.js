08504e6381540578f5f48143f17c74c2dc5ab2e6
Julie Altman <jtskolnik@gmail.com>
2021-02-08 10:16:01 -0500

    made code more dry

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -11,12 +10,8 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 const employeeArray = [];
 
-//All classes need name, id, and email
 const promptUser = async () => {
     try {
         //Make an object for each team member depending on what class it is
@@ -26,89 +21,63 @@ const promptUser = async () => {
                 message: 'What is the role of this employee?',
                 name: 'role',
                 choices: ["Engineer", "Intern", "Manager"]
-            }
-
+            },
+            {
+                type: 'input',
+                message: 'What is the name of the employee?',
+                name: 'name'
+            },
+            {
+                type: 'input',
+                message: "What is the employee's id number?",
+                name: 'id',
+                //Verify that it is a number
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                type: 'input',
+                message: "What is the employee's email address?",
+                name: 'email',
+                validate: function (value) {
+                    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
         ]).then(function (answer) {
+            let oneName = answer.name;
+            let oneId = answer.id;
+            let oneEmail = answer.email;
+
             if (answer.role == "Manager") {
                 //Manager needs office number
                 inquirer
                     .prompt([
                         {
                             type: 'input',
-                            message: 'What is the name of the employee?',
-                            name: 'name'
-                        },
-                        {
-                            type: 'input',
-                            message: "What is the employee's id number?",
-                            name: 'id',
-                            //Verify that it is a number
-                            validate: function(value) {
+                            message: "What is the manager's office number?",
+                            name: 'officenumber',
+                            validate: function (value) {
                                 if (isNaN(value) === false) {
-                                  return true;
-                                }
-                                return false;
-                              }
-                        },
-                        {
-                            type: 'input',
-                            message: "What is the employee's email address?",
-                            name: 'email',
-                            validate: function(value){
-                                if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)){
                                     return true;
                                 }
                                 return false;
                             }
-                        },
-                        {
-                            type: 'input',
-                            message: "What is the manager's office number?",
-                            name: 'officenumber',
-                            validate: function(value) {
-                                if (isNaN(value) === false) {
-                                  return true;
-                                }
-                                return false;
-                              }
                         }
                     ]).then(answer => {
-
-                        let newEmployee = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
+                        let newEmployee = new Manager(oneName, oneId, oneEmail, answer.officeNumber);
                         employeeArray.push(newEmployee);
                         startOver(employeeArray);
-
                     })
             } else if (answer.role == "Engineer") {
                 inquirer
                     .prompt([
-                        {
-                            type: 'input',
-                            message: 'What is the name of the employee?',
-                            name: 'name'
-                        },
-                        {
-                            type: 'input',
-                            message: "What is the employee's id number?",
-                            name: 'id',
-                            validate: function(value) {
-                                if (isNaN(value) === false) {
-                                  return true;
-                                }
-                                return false;
-                              }
-                        },
-                        {
-                            type: 'input',
-                            message: "What is the employee's email address?",
-                            name: 'email',
-                            validate: function(value){
-                                if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)){
-                                    return true;
-                                }
-                                return false;
-                            }
-                        },
                         {
                             type: 'input',
                             message: "What is the engineer's github username?",
@@ -116,52 +85,22 @@ const promptUser = async () => {
                         }
                     ]).then(function (answer) {
                         //Engineer also needs github username
-                        let newEmployee = new Engineer(answer.name, answer.id, answer.email, answer.github_username);
+                        let newEmployee = new Engineer(oneName, oneId, oneEmail, answer.github_username);
                         employeeArray.push(newEmployee);
 
                         startOver(employeeArray);
                     })
-            } else  {
+            } else {
                 inquirer
                     .prompt([
-                        {
-                            type: 'input',
-                            message: 'What is the name of the employee?',
-                            name: 'name'
-                        },
-                        {
-                            type: 'input',
-                            message: "What is the employee's id number?",
-                            name: 'id',
-                            validate: function(value) {
-                                if (isNaN(value) === false) {
-                                  return true;
-                                }
-                                return false;
-                              }
-                        },
-                        {
-                            type: 'input',
-                            message: "What is the employee's email address?",
-                            name: 'email',
-                            validate: function(value){
-                                if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)){
-                                    return true;
-                                }
-                                return false;
-                            }
-                        },
                         {
                             type: 'input',
                             message: "What school does the intern attend?",
                             name: 'school'
                         }
                     ]).then(answer => {
-
-                        let newEmployee = new Intern(answer.name, answer.id, answer.email, answer.school);
-
+                        let newEmployee = new Intern(oneName, oneId, oneEmail, answer.school);
                         employeeArray.push(newEmployee);
-
                         startOver(employeeArray);
                     });
             }
@@ -172,7 +111,6 @@ const promptUser = async () => {
 }
 
 promptUser();
-
 
 const startOver = array => {
     inquirer
@@ -193,22 +131,3 @@ const startOver = array => {
 }
 
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
